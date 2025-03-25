5074422641783d702f36727fd878a34288e8b271
Caruh <miso4206@gmail.com>
2025-03-25 11:16:28 +0900

    [FEAT] 초기설정

---
+++ src/main/java/ai/Mayi/MayiApplication.java
@@ -0,0 +1,13 @@
+package ai.Mayi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayiApplication.class, args);
+	}
+
+}
