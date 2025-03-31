ed6fbb68821f755b19085fd5b11dd2eee72e9a36
Caruh <miso4206@gmail.com>
2025-03-31 11:31:48 +0900

    [REFACTOR] #3 회원가입 기능 수정

--- src/main/java/ai/Mayi/web/controller/UserController.java
+++ src/main/java/ai/Mayi/web/controller/UserController.java
@@ -1,24 +1,35 @@
 package ai.Mayi.web.controller;
-import ai.Mayi.repository.UserRepository;
-import ai.Mayi.service.UserService;
+//import ai.Mayi.service.JwtUtil;
+//import ai.Mayi.service.UserServiceImpl;
+import ai.Mayi.service.JwtUtil;
+import ai.Mayi.service.UserServiceImpl;
 import ai.Mayi.web.dto.UserDTO;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Slf4j
-@Controller
 @RequiredArgsConstructor
+@Controller
 public class UserController {
-    private final UserRepository userRepository;
-    private final UserService userService;
+    private final UserServiceImpl userserviceImpl;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @GetMapping("/login")
     public String login() {
         return "login.html";
     }
+
 
     @GetMapping("/register")
     public String register() {
@@ -28,9 +39,9 @@ public class UserController {
 
     @ResponseBody
     @PostMapping("/register")
-    public String user_register(@RequestBody UserDTO.UserRequestDTO userDto) {
-        log.info("Registering post user: {}", userDto.toString());
-        userService.signUp(userDto);
+    public String user_register(@RequestBody UserDTO.JoinRequestDTO joinDto) throws Exception {
+        log.info("Registering post user: {}", joinDto.toString());
+        userserviceImpl.signUp(joinDto);
         return "good";
     }
 }
