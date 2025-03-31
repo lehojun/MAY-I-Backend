4921ca23629b48463676a2b1f6ebdb6ae9e7324e
Caruh <miso4206@gmail.com>
2025-03-31 11:22:38 +0900

    [FEAT] #3 유저 이메일 찾기 메서드 추가

--- src/main/java/ai/Mayi/repository/UserRepository.java
+++ src/main/java/ai/Mayi/repository/UserRepository.java
@@ -3,7 +3,9 @@ package ai.Mayi.repository;
 import ai.Mayi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserEmail(String userEmail);
 }
