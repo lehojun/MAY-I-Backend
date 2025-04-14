64ff96e9eb6b492fe57432cb6ec0c382c33dc1fe
Caruh <miso4206@gmail.com>
2025-04-14 16:06:29 +0900

    [FIX] JWT 생성 오류 수정

--- src/main/java/ai/Mayi/jwt/CookieUtil.java
+++ src/main/java/ai/Mayi/jwt/CookieUtil.java
@@ -11,7 +11,7 @@ public class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
-//        cookie.setSecure(true); // only https
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
