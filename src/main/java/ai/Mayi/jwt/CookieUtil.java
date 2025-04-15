1e3c1c37f5df2d909cd6a27c6ddf03e8dec8b49a
Caruh <miso4206@gmail.com>
2025-04-15 13:24:32 +0900

    [FEAT] 쿠키 도메인 설정

--- src/main/java/ai/Mayi/jwt/CookieUtil.java
+++ src/main/java/ai/Mayi/jwt/CookieUtil.java
@@ -11,8 +11,9 @@ public class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false); //https환경이 아니라 나중에 바뀌면 true로 변경
         cookie.setPath("/");
+        cookie.setDomain("localhost"); //나중에 배포 주소로 바꿀 것.
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
